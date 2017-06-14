fb57bcc91bfc6ce0f409239571cb378affa18ca5
Martian <martionlee@gmail.com>
2017-06-14 12:56:02 +0900

    add about page.

---
+++ pages/about.vue
@@ -0,0 +1,40 @@
+<template>
+    <section class="container">
+        <h1>About page</h1>
+        <p>Check the source code to see the custom meta tags added with our custom component <code>head-card</code></p>
+        <head-card></head-card>
+        <p>
+            This Diary is made by Seonghwa Lee.
+            He loves the world and he wants to change the world more lovely.
+        </p>
+        <div class="links">
+            <nuxt-link to="/" class="button--green">Home</nuxt-link>
+            <a href="https://nuxtjs.org/" target="_blank" class="button--grey">Documentation</a>
+            <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        </div>
+    </section>
+</template>
+
+<script>
+  import HeadCard from '~components/head-card.vue'
+
+  export default {
+    head: {
+      title: 'About Page',
+      meta: [
+        { hid: 'description', name: 'description', content: 'About page description' }
+      ]
+    },
+    components: {
+      HeadCard
+    }
+  }
+</script>
+
+<style scoped>
+    .container {
+        text-align: center;
+        margin-top: 150px;
+        font-size: 20px;
+    }
+</style>
